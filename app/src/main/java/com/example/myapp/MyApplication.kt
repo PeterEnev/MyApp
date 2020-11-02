a826391bff18594152dfbd24b1363e56c5ca6067
PetarEnev <peterenev@gmail.com>
2020-11-02 16:58:16 +0200

    fix

--- app/src/main/java/com/example/myapp/MyApplication.kt
+++ app/src/main/java/com/example/myapp/MyApplication.kt
@@ -1,8 +1,6 @@
 package com.example.myapp
 
 import android.app.Application
-import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatDelegate
 
 
 class MyApplication : Application() {
